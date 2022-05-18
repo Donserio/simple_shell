2ec8273eee84c4ea4be00920f85ac77bcac02a32
Donserio <donserio4real.com>
2022-05-18 16:07:32 -0700

    simple shell is not

---
+++ main.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+/**
+ * main - main function
+ * Return: nothing
+ */
+int main(void)
+{
+char *line = NULL;
+char **args;
+size_t size = 0;
+int c;
+do {
+if (isatty(STDIN_FILENO) == 1)
+{
+_putchar('$');
+_putchar(' ');
+}
+c = getline(&line, &size, stdin);
+if (c == -1)
+{
+free(line);
+exit(0);
+}
+if (line[0] == '\n')
+{
+continue;
+}
+args = tokenizer(line);
+if (*args[0] == ' ')
+{
+free(line);
+free(args);
+break;
+}
+
+shell_builtin(args, line);
+free(args);
+free(line);
+size = 0;
+} while (1);
+return (0);
+}
+
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strcat - gkjsgkjw;
+ * @dest: kfdjkla
+ * @src: fdjkkfjal
+ *
+ * Return: lkjdalk;f
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	for (len = 0; dest[len] != 0;)
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+		dest[len + i] = src[i];
+	return (dest);
+}
---
+++ shell.h
@@ -0,0 +1,59 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <errno.h>
+#include <stddef.h>
+#define BUFSIZE 1024
+
+int shell_builtin(char **args, char *line);
+void shell_run(char **args, char *line);
+int _strcmp(char *s1, char *s2);
+typedef void sigfunc(int);
+void sig_handler(int signo);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+
+/* parser.c */
+
+/*char **parser(char *string, char *delim);*/
+char **parser(char *line);
+void fork_exec(char *path, char **token, char **env);
+char *com_remover(char *path);
+int double_space_remover(char *path);
+unsigned int _wordCount(char *str);
+int _strlen(char *str);
+int *_astrlen(char **str);
+char *_strdup(char *source, unsigned int extra);
+char **tokenizer(char *buffer);
+
+/**
+ * struct built_s - linked list of builtins
+ * @name: name of builtin
+ * @p: pointer to function
+ *
+ * Description: struct for builtin functions.
+**/
+typedef struct built_s
+{
+	char *name;
+	int (*p)(void);
+
+} built_s;
+
+extern char **environ;
+
+#endif
