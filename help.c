272e81a8bea19ce607a323fc65aac52f3bb119a3
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-18 13:40:55 +0100

    Update help.c

--- help.c
+++ help.c
@@ -25,7 +25,7 @@ void free_args(char **args, char **front)
  * Description: Opens the stat file, a space-delimited file containing
  *              information about the current process. The PID is the
  *              first word in the file. The function reads the PID into
- *              a buffer and replace the space at the end with a \0 byte.
+ *              a buffer, and replace the space at the end with a \0 byte.
  *
  * Return: The current process ID or NULL on failure.
  */
