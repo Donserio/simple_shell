2bb58409a446f3925d38aa66505ef3ee34d874f8
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-18 21:59:06 +0100

    Update strot2.c

--- strot2.c
+++ strot2.c
@@ -1,105 +1,33 @@
 #include "shell.h"
 
-char *_strchr(char *s, char c);
-int _strspn(char *s, char *accept);
-int _strcmp(char *s1, char *s2);
-int _strncmp(const char *s1, const char *s2, size_t n);
-
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
+ * exit_sh - exits the shell
  *
- * Return: If c is found - a pointer to the first occurence.
- *         If c is not found - NULL.
+ * @input: vector of inputed arguments
+ * @cmd: struct of global variables
+ * Return: 0 on success.
  */
-char *_strchr(char *s, char c)
+int exit_sh(char **input, cmd_t *cmd)
 {
-	int index;
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
 
-	for (index = 0; s[index]; index++)
+	if (input[1])
 	{
-		if (s[index] == c)
-			return (s + index);
-	}
-
-	return (NULL);
-}
-
-/**
- * _strspn - Gets the length of a prefix substring.
- * @s: The string to be searched.
- * @accept: The prefix to be measured.
- *
- * Return: The number of bytes in s which
- *         consist only of bytes from accepts.
- */
-int _strspn(char *s, char *accept)
-{
-	int bytes = 0;
-	int index;
-
-	while (*s)
-	{
-		for (index = 0; accept[index]; index++)
+		ustatus = atoi(input[1]);
+		is_digit = _isdigit(input[1]);
+		str_len = strlen(input[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
 		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
+			t_error("Invalid exit status");
+			cmd->status = 2;
 		}
-		s++;
+		cmd->status = (ustatus % 256);
+		cmd->ready = 0;
 	}
-	return (bytes);
-}
-
-/**
- * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
- *
- * Return: Positive byte difference if s1 > s2
- *         0 if s1 = s2
- *         Negative byte difference if s1 < s2
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-
-	if (*s1 != *s2)
-		return (*s1 - *s2);
-
-	return (0);
-}
-
-/**
- * _strncmp - Compare two strings.
- * @s1: Pointer to a string.
- * @s2: Pointer to a string.
- * @n: The first n bytes of the strings to compare.
- *
- * Return: Less than 0 if s1 is shorter than s2.
- *         0 if s1 and s2 match.
- *         Greater than 0 if s1 is longer than s2.
- */
-int _strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t i;
-
-	for (i = 0; s1[i] && s2[i] && i < n; i++)
-	{
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-	}
-	if (i == n)
-		return (0);
-	else
-		return (-15);
+	cmd->ready = 0;
+	return (1);
 }
