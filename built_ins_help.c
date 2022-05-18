26dd0cb9251fba1ea8df3dd10edf7e3a70a9a92b
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-18 21:52:52 +0100

    Update built_ins_help.c

--- built_ins_help.c
+++ built_ins_help.c
@@ -1,61 +1,36 @@
 #include "shell.h"
 
-
-	char **commands = NULL;
-	char *line = NULL;
-	char *shell_name = NULL;
-	int status = 0;
-
 /**
- * main - the main shell code
- * @argc: number of arguments passed
- * @argv: program arguments to be parsed
- *
- * applies the functions in utils and helpers
- * implements EOF
- * Prints error on Failure
- * Return: 0 on success
+ * _realloc -reallocate memory
+ * @ptr: pointer pointing to the memory
+ * 
+ * Return: the pointer pointing to the memory address
  */
-
-
-int main(int argc __attribute__((unused)), char **argv)
+void *_realloc(void *ptr, size_t originalLength, size_t newLength)
 {
-	char **current_command = NULL;
-	int i, type_command = 0;
-	size_t n = 0;
 
-	signal(SIGINT, ctrl_c_handler);
-	shell_name = argv[0];
-	while (1)
-	{
-		non_interactive();
-		print(" ($) ", STDOUT_FILENO);
-		if (getline(&line, &n, stdin) == -1)
-		{
-			free(line);
-			exit(status);
-		}
-			remove_newline(line);
-			remove_comment(line);
-			commands = tokenizer(line, ";");
-
-		for (i = 0; commands[i] != NULL; i++)
-		{
-			current_command = tokenizer(commands[i], " ");
-			if (current_command[0] == NULL)
-			{
-				free(current_command);
-				break;
-			}
-			type_command = parse_command(current_command[0]);
-
-			/* initializer -   */
-			initializer(current_command, type_command);
-			free(current_command);
-		}
-		free(commands);
-	}
-	free(line);
-
-	return (status);
+   if (newLength == 0)
+   {
+      free(ptr);
+      return NULL;
+   }
+   else if (!ptr)
+   {
+      return malloc(newLength);
+   }
+   else if (newLength <= originalLength)
+   {
+      return ptr;
+   }
+   else
+   {
+      assert((ptr) && (newLength > originalLength));
+      void *ptrNew = malloc(newLength);
+      if (ptrNew)
+      {
+          memcpy(ptrNew, ptr, originalLength);
+          free(ptr);
+      }
+      return ptrNew;
+    }
 }
