3a6ea004d822ba01f8361367d5958a43116b4d49
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-18 21:51:44 +0100

    Update built_in.c

--- built_in.c
+++ built_in.c
@@ -1,174 +1,105 @@
 #include "shell.h"
 
 /**
- * Auth: Ekene Okoli
- * Esther Ashinedu Uvo
- *
- * Description:
- * the extended functions for main.c
+ * assign_lineptr - Reassigns the lineptr variable for _getline.
+ * @lineptr: A buffer to store an input string.
+ * @n: The size of lineptr.
+ * @buffer: The string to assign to lineptr.
+ * @b: The size of buffer.
  */
-
-
-/** parse_command - determines the type of the command
- * @command: command to be parsed
- *
- * Return: constant representing the type of the command
- * Description -
- * 		 EXTERNAL_COMMAND (1) represents commands like /bin/ls
- *		 INTERNAL_COMMAND (2) represents commands like exit, env
- *		 PATH_COMMAND (3) represents commands found in the PATH like ls
- *		 INVALID_COMMAND (-1) represents invalid commands
- */
-
-int parse_command(char *command)
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 {
-	int i;
-	char *internal_command[] = {"env", "exit", NULL};
-	char *path = NULL;
-
-	for (i = 0; command[i] != '\0'; i++)
+	if (*lineptr == NULL)
 	{
-		if (command[i] == '/')
-			return (EXTERNAL_COMMAND);
+		if (b > 120)
+			*n = b;
+		else
+			*n = 120;
+		*lineptr = buffer;
 	}
-	for (i = 0; internal_command[i] != NULL; i++)
+	else if (*n < b)
 	{
-		if (_strcmp(command, internal_command[i]) == 0)
-			return (INTERNAL_COMMAND);
+		if (b > 120)
+			*n = b;
+		else
+			*n = 120;
+		*lineptr = buffer;
 	}
-	/* @check_path - checks if a command is found in the PATH */
-	path = check_path(command);
-	if (path != NULL)
+	else
 	{
-		free(path);
-		return (PATH_COMMAND);
-	}
-
-	return (INVALID_COMMAND);
-}
-
-/**
- * execute_command - executes a command based on it's type
- * @tokenized_command: tokenized form of the command (ls -l == {ls, -l, NULL})
- * @command_type: type of the command
- *
- * Return: void
- */
-void execute_command(char **tokenized_command, int command_type)
-{
-	void (*func)(char **command);
-
-	if (command_type == EXTERNAL_COMMAND)
-	{
-		if (execve(tokenized_command[0], tokenized_command, NULL) == -1)
-		{
-			perror(_getenv("PWD"));
-			exit(2);
-		}
-	}
-	if (command_type == PATH_COMMAND)
-	{
-		if (execve(check_path(tokenized_command[0]), tokenized_command, NULL) == -1)
-		{
-			perror(_getenv("PWD"));
-			exit(2);
-		}
-	}
-	if (command_type == INTERNAL_COMMAND)
-	{
-		func = get_func(tokenized_command[0]);
-		func(tokenized_command);
-	}
-	if (command_type == INVALID_COMMAND)
-	{
-		print(shell_name, STDERR_FILENO);
-		print(": 1: ", STDERR_FILENO);
-		print(tokenized_command[0], STDERR_FILENO);
-		print(": not found\n", STDERR_FILENO);
-		status = 127;
+		strcpy(*lineptr, buffer);
+		free(buffer);
 	}
 }
 
 /**
- * check_path - checks if a command is found in the PATH
- * @command: command to be used
+ * _getline - Reads input from a stream.
+ * @lineptr: A buffer to store the input.
+ * @n: The size of lineptr.
+ * @stream: The stream to read from.
  *
- * Return: path where the command is found in, NULL if not found
+ * Return: The number of bytes read.
  */
-char *check_path(char *command)
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	char **path_array = NULL;
-	char *temp, *temp2, *path_cpy;
-	char *path = _getenv("PATH");
-	int i;
+	static ssize_t input;
+	ssize_t ret;
+	char c = 'x';
+	char *buffer;
+	int r;
 
-	if (path == NULL || _strlen(path) == 0)
-		return (NULL);
-	path_cpy = malloc(sizeof(*path_cpy) * (_strlen(path) + 1));
-	_strcpy(path, path_cpy);
-	path_array = tokenizer(path_cpy, ":");
-	for (i = 0; path_array[i] != NULL; i++)
+	if (input == 0)
 	{
-		temp2 = _strcat(path_array[i], "/");
-		temp = _strcat(temp2, command);
-		if (access(temp, F_OK) == 0)
+		fflush(stream);
+	}
+	else
+	{
+		return (-1);
+	}
+
+	input = 0;
+
+	//Allocating memory
+	buffer = malloc(sizeof(char) * 120);
+
+	// if memory allocation fails
+	if (!buffer)
+	{
+		return (-1);
+	}
+
+	//Initialize a loop to read all char if c is not newline
+	while (c != '\n')
+	{
+		r = read(STDIN_FILENO, &c, 1);
+		if (r == -1 || (r == 0 && input == 0))
 		{
-			free(temp2);
-			free(path_array);
-			free(path_cpy);
-			return (temp);
+			free(buffer);
+			return (-1);
 		}
-		free(temp);
-		free(temp2);
-	}
-	free(path_cpy);
-	free(path_array);
-	return (NULL);
-}
-
-/**
- * get_func - retrieves a function based on the command given and a mapping
- * @command: string to check against the mapping
- *
- * Return: pointer to the proper function, or null on fail
- */
-void (*get_func(char *command))(char **)
-{
-	int i;
-	function_map mapping[] = {
-		{"env", env}, {"exit", quit}
-	};
-
-	for (i = 0; i < 2; i++)
-	{
-		if (_strcmp(command, mapping[i].command_name) == 0)
-			return (mapping[i].func);
-	}
-	return (NULL);
-}
-
-/**
- * _getenv - gets the value of an environment variable
- * @name: name of the environment variable
- *
- * Return: the value of the variable as a string
- */
-char *_getenv(char *name)
-{
-	char **my_environ;
-	char *pair_ptr;
-	char *name_cpy;
-
-	for (my_environ = environ; *my_environ != NULL; my_environ++)
-	{
-		for (pair_ptr = *my_environ, name_cpy = name;
-		     *pair_ptr == *name_cpy; pair_ptr++, name_cpy++)
+		if (r == 0 && input != 0)
 		{
-			if (*pair_ptr == '=')
-				break;
+			input++;
+			break;
 		}
-		if ((*pair_ptr == '=') && (*name_cpy == '\0'))
-			return (pair_ptr + 1);
+		if (input >= 120)
+		{
+			buffer = realloc(buffer, input + 1);
+		}
+
+		buffer[input] = c;
+		input++;
 	}
-	return (NULL);
+
+	//add newline at the end of the buffer
+	buffer[input] = '\n';
+
+	assign_lineptr(lineptr, n, buffer, input);
+
+	ret = input;
+	if(r != 0)
+	{
+		input = 0;
+	}
+	return (ret);
 }
