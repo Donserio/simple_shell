b2f73a7a48349ecfa65bda976d62a2b5b184f351
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-18 16:18:11 +0100

    Update built_ins_help_2.c

--- built_ins_help_2.c
+++ built_ins_help_2.c
@@ -1,45 +1,62 @@
 #include "shell.h"
 
-void help_env(void);
-void help_setenv(void);
-void help_unsetenv(void);
-void help_history(void);
-
 /**
- * help_env - Displays information on the hsh builtin commands 'env'.
+ *env - prints the current_environnement
+ *@tokenized_command: command entered
+ *
+ *Return: void
  */
-void help_env(void)
-{
-	char *msg = "env: env\n\tPrints the current environment.\n";
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
+void env(char **tokenized_command __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
+	}
 }
 
 /**
- * help_setenv - Displays information on the hsh builtin command 'setenv'.
+ * quit - exits the shell
+ * @tokenized_command: command entered
+ *
+ * Return: void
  */
-void help_setenv(void)
+
+void quit(char **tokenized_command)
 {
-	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+	int num_token = 0, arg;
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environment variable, or modifies an existing one.\n\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\tUpon failure, prints a message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-}
-
-/**
- * help_unsetenv - Displays information on the hsh builtin command
- * 'unsetenv'.
- */
-void help_unsetenv(void)
-{
-	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
-
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environmental variable.\n\n\tUpon failure, prints a ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	for (; tokenized_command[num_token] != NULL; num_token++)
+		;
+	if (num_token == 1)
+	{
+		free(tokenized_command);
+		free(line);
+		free(commands);
+		exit(status);
+	}
+	else if (num_token == 2)
+	{
+		arg = _atoi(tokenized_command[1]);
+		if (arg == -1)
+		{
+			print(shell_name, STDERR_FILENO);
+			print(": 1: exit: Illegal number: ", STDERR_FILENO);
+			print(tokenized_command[1], STDERR_FILENO);
+			print("\n", STDERR_FILENO);
+			status = 2;
+		}
+		else
+		{
+			free(line);
+			free(tokenized_command);
+			free(commands);
+			exit(arg);
+		}
+	}
+	else
+		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 }
