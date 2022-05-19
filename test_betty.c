bb58576714275c366d6fdf833ab81a8a7469a957
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-19 02:25:08 +0100

    Update test_betty.c

--- test_betty.c
+++ test_betty.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 #include "shell.h"
+/**
+ *main - A program to write to the user
+ *Return: an integer 1 always
+ */
 
-int dear(void)
-{  
-   write(2,"Hello world!", 12);
-   return (1);
-}  
+int dear_betty(void)
+{
+        write(2, "This is our first Shell project", 32);
+        return (1);
+}
