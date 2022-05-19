0bbcca72e33de0fe9fcff11137e3834e65805521
Donserio <49341396+Donserio@users.noreply.github.com>
2022-05-19 02:16:10 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -16,6 +16,7 @@
 #include <errno.h>
 #include <stddef.h>
 #define BUFSIZE 1024
+#define start main
 
 int shell_builtin(char **args, char *line);
 void shell_run(char **args, char *line);
@@ -24,7 +25,6 @@ typedef void sigfunc(int);
 void sig_handler(int signo);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-int dear(void);
 
 
 /* parser.c */
