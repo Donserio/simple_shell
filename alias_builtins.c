05a19bdc03e4beedef86f00024557a10178f9407
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-05-20 01:11:51 +0100

    Update alias_builtins.c

--- alias_builtins.c
+++ alias_builtins.c
@@ -13,7 +13,7 @@ void print_alias(alias_t *alias);
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
  */
-int hsh(char **args, char __attribute__((__unused__)) **front)
+int hsh_alias(char **args, char __attribute__((__unused__)) **front)
 {
 	alias_t *temp = aliases;
 	int i, ret = 0;
